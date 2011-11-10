511c2dd7e1adab67bd3c6dd90f4ec9c963f6e1fe
garcia95721 <garcia95721@gmail.com>
2011-11-10 12:31:45 +0000

    Proyecto funcional.

---
+++ src/practicas/Main.java
@@ -0,0 +1,13 @@
+package practicas;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("Hola Grupo.");//Hola chicos ya está probado e implementado el repo, solo queda empezar a programar =).
+	}
+
+}
