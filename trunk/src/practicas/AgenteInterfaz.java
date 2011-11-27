f0307f7979d906b14c7e5351e9e68fe70188eedb
garcia95721@gmail.com <garcia95721@gmail.com@f20cbfb9-18a3-a564-c733-b528d7ed5322>
2011-11-27 00:17:38 +0000

    Interfaz implementada con un layout y correción de comentarios y modularización.

--- trunk/src/practicas/AgenteInterfaz.java
+++ trunk/src/practicas/AgenteInterfaz.java
@@ -2,27 +2,97 @@ package practicas;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.*;
 
-public class AgenteInterfaz {
-	
-	JFrame frame;
-	
+public class AgenteInterfaz extends JFrame implements ActionListener {
+
+	private JLabel urlLabel;
+	private JTextField campoUrlText;
+
+	private JLabel keywordLabel;
+	private JTextField campoKeywordText;
+
+	private JButton empezarButtom;
+	private JButton salirButtom;
+
 	public AgenteInterfaz() {
+		super("Practicas Multiagente");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(450, 300);
+		//setResizable(false);// Para fijar la pantalla.
+		definirVentana();
+		setVisible(true);
+	}
+
+	public void definirVentana() {
 		
+		setLayout(new GridBagLayout());
+		urlLabel = new JLabel("URL:");
+		generarRestriccion(urlLabel, 0, 0, 1, 1, 0, 0);
 		
-		frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Practicas Multiagente");
-        frame.setBounds(275,150,450,300);
-        
-        Principal panel = new Principal();
-        
-        
-        frame.add(panel);
-        frame.setVisible(true);
+		campoUrlText = new JTextField();
+		generarRestriccion(campoUrlText, 1, 0, 2, 3, 0,0);	
+		
+		keywordLabel = new JLabel("Keyword:");
+		generarRestriccion(keywordLabel, 0, 2, 1, 2, 0, 0);
+		
+		campoKeywordText = new JTextField();
+		generarRestriccion(campoKeywordText, 1, 2, 2, 3, 0, 0);	
+
+		empezarButtom = new JButton("Comenzar");
+		generarRestriccion(empezarButtom, 1, 5, 1, 1, 0, 0);			
+		
+		salirButtom = new JButton("Salir");
+		generarRestriccion(salirButtom, 2, 5, 1, 1, 0, 0);
+
+		empezarButtom.addActionListener(this);
+		salirButtom.addActionListener(this);
 	}
 	
-	
+	/**
+	 * Método para generar una restricción para el layout.
+	 * @param comp
+	 * @param gridx
+	 * @param gridy
+	 * @param gridwidth
+	 * @param gridheight
+	 * @param weightx
+	 * @param weighty
+	 * @return
+	 */
+	public GridBagConstraints generarRestriccion(Component comp,int gridx, int gridy, int gridwidth, int gridheight, int weightx, int weighty ){
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx=gridx;
+		gbc.gridy=gridy;
+		gbc.gridwidth=gridwidth;
+		gbc.gridheight=gridheight;
+		gbc.weightx=weightx;
+		gbc.weighty=weighty;
+		gbc.fill= GridBagConstraints.BOTH;
+		add(comp,gbc);
+		return gbc;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == empezarButtom) {
+			AgenteBusqueda agent;
+			try {
+				String pruebaURL = campoUrlText.getText();
+				if (pruebaURL.startsWith("www"))
+					pruebaURL = "http://" + pruebaURL;
+				agent = new AgenteBusqueda("1", new URL(pruebaURL), campoKeywordText.getText().split(","));
+				agent.start();
+			} catch (MalformedURLException e1) {
+				// TODO: mostrar error
+			}
+
+		} else if (e.getSource() == salirButtom) {
+			System.exit(0);
+		}
+
+	}
 }
