3a35f45d7eb145aa62cb59881a6d3dbef2233a9f
garcia95721@gmail.com <garcia95721@gmail.com@f20cbfb9-18a3-a564-c733-b528d7ed5322>
2011-12-06 21:49:14 +0000

    Creadas las dos lineas de trabajo

---
+++ branches/Linea2/branches/Linea2/trunk/src/comunicacion/LogFile.java
@@ -0,0 +1,61 @@
+package comunicacion;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import practicas.AgenteInterfaz;
+
+public class LogFile {
+
+	private BufferedWriter out;
+	private String path;
+	
+	public LogFile(String p){
+		 path = p;
+		 
+		 try {
+			 
+			out = new BufferedWriter(new FileWriter(path));
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	 
+	}
+
+	
+	public synchronized void escribir(String s) {
+		
+		try {
+			
+			out.write(s);
+			AgenteInterfaz.escribirTextArea(s);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void reiniciar() {
+		
+		cerrar();
+		try {
+			 
+			out = new BufferedWriter(new FileWriter(path));
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	public void cerrar() {
+		try {
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
